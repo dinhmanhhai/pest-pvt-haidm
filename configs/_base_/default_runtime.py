803347609fc98e4788efa103f3b6798f1ddc219d
Manh Hai <hai.manh.dinh@gmail.com>
2026-05-23 14:19:51 +0700

    init project wandb

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -4,7 +4,18 @@ log_config = dict(
     interval=50,
     hooks=[
         dict(type='TextLoggerHook'),
-        # dict(type='TensorboardLoggerHook')
+        # dict(type='TensorboardLoggerHook'),
+        dict(
+            type='WandbLoggerHook',
+            init_kwargs=dict(
+                project='pest-pvt',
+                name='atss_pvtv2_dyhead3_ass_8gpu',
+                entity=None,
+            ),
+            interval=50,
+            log_checkpoint=False,
+            log_checkpoint_metadata=True,
+            num_eval_images=20),
     ])
 # yapf:enable
 custom_hooks = [dict(type='NumClassCheckHook')]
